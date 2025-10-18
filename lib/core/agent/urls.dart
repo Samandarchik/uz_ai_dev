0bee7c1a413481e69c38827ba038d3a9c5b01c72
Samandar Ibragimov <samandaik4@gmail.com>
2025-10-18 07:30:50 +0500

    0.1.6

--- lib/core/agent/urls.dart
+++ lib/core/agent/urls.dart
@@ -1,6 +1,6 @@
 abstract final class AppUrlsAgent {
-  // static const String baseUrl = "https://agent.monebakeryuz.uz";
-  static const baseUrl = "http://localhost:1010";
+  static const String baseUrl = "https://agent.monebakeryuz.uz";
+  // static const baseUrl = "http://localhost:1010";
   static const String login = '$baseUrl/api/login';
   static const String register = '$baseUrl/api/register';
   static const String refresh = '$baseUrl/api/auth/refresh';
